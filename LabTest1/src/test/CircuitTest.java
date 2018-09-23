9b4d5eff5e4abd84c36f622ccdc7a2b6c083612c
Rubel2475 <37175213+Rubel2475@users.noreply.github.com>
2018-09-23 17:38:38 +0600

    Add files via upload

---
+++ LabTest1/src/test/CircuitTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.Circuit;
+
+
+class CircuitTest {
+
+	@Test
+	void circuitTest() {
+		Circuit circuit = new Circuit();
+	}
+
+}
