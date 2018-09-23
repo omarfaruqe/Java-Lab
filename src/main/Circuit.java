3466d2f0fa0a4d4db0ad36913d7d49843c78aa00
Susmoy Barman <susmoy11223300@gmail.com>
2018-09-23 15:32:09 +0600

    solution for the circuit given in the question.

--- src/main/Circuit.java
+++ src/main/Circuit.java
@@ -1,5 +1,32 @@
 package main;
 
-public class Circuit {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Circuit {
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		File file = new File("D:\\Academic\\2-2\\Java_Practice\\eclipse\\Lab Test 1\\src\\input.txt"); 
+		Scanner sc = new Scanner(file); 
+			  
+		if (sc.hasNextDouble()) {
+			double t0 = sc.nextInt();
+			double tn = sc.nextInt();
+			
+			double B = sc.nextDouble();
+			double R = sc.nextDouble();
+			double C = sc.nextDouble();
+			double e = 2.7183;
+			double rc = 1/(R*C);
+			
+			double T = tn/100;
+			for(double i = t0; i<=tn; i=i+T) {
+				double ep = 1 / (Math.pow(e, (rc*i)));
+				double v = B*(1-ep);
+				System.out.println(i+"  "+v);
+			}
+		}	      
+	}
 }
+
