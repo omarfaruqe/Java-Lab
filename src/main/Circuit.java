b9d0d4735f052cbae439a88338411ee8ae5dbaa4
Apple Mahmud <applecse25@gmail.com>
2018-09-23 21:50:32 +0600

    Complete program upload

--- src/main/Circuit.java
+++ src/main/Circuit.java
@@ -1,5 +1,40 @@
 package main;
+//import java.
+import java.lang.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Circuit {
+	public static void main(String arg[])
+	{
+		try {
+			FileReader input=new FileReader("params.txt");
+			Scanner scan=new Scanner(input);
+			PrintWriter write=new PrintWriter("rc.txt");
+			double B,R,C,voltage;
+			int t1,t2,step;
+			B=scan.nextDouble();
+			C=scan.nextDouble();
+			R=scan.nextDouble();
+			t1=scan.nextInt();
+			t2=scan.nextInt();
+			step=(t2-t1)/100;
+			for(int i=0;i<100;i++)
+			{
+				voltage=12*(1-Math.exp(-.008*step));
+				write.print(step);
+				write.print("    ");
+				write.println(voltage);
+				step=step+10;
+			}
+			write.close();
+			scan.close();
+		} 
+		catch (IOException x) {
+		   // System.err.format("IOException: %s%n", x);
+		}
+	}
 
 }
