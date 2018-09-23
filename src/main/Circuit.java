2901ddc6da1023ac01963b142328eca46c2de4e4
SohanCSERU <israkcse@gmail.com>
2018-09-23 16:10:29 +0600

    Write The Function

--- src/main/Circuit.java
+++ src/main/Circuit.java
@@ -1,5 +1,22 @@
 package main;
 
-public class Circuit {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public static double Back(int B,int R,int C,int t)
+{
+	double volt = B * (1 - Math.pow(e, (-t / (double)(R * C))));
+	return volt;
+}
+
+public class Test {
+	public static void main(String[] args) throw FileNotFoundException {
+		
+		Scanner in = new Scanner(new File("params.txt"));
+		
+
+	}
 
 }
