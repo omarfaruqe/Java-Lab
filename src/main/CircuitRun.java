bddbc1941b92085a24554b9cf6d5f4508d4f9afd
Arupcseru <arupcseru@gmail.com>
2018-09-23 17:36:07 +0600

    Adding for loop and necessary code for reading from a file and write in another one.Everything is ok.

---
+++ src/main/CircuitRun.java
@@ -0,0 +1,12 @@
+package main;
+
+public class CircuitRun {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circuit circuit = new Circuit();
+		circuit.calculateVoltage();
+
+	}
+
+}
