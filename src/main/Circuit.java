25efbe75e0fa6874a9ff4b439f34828101a11cd8
Anik-Modak <anikmodak.rucse@gmail.com>
2018-09-23 16:09:03 +0600

    This is my final solution

--- src/main/Circuit.java
+++ src/main/Circuit.java
@@ -2,48 +2,61 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Circuit 
 {
-	public static void main(String[] args) throws FileNotFoundException 
+	public static void main(String[] args) 
 	{
 		
-		Scanner console = new Scanner(System.in);
-		
-		System.out.print("Input file: ");
-		String inputFileName = console.next();
-		
-		System.out.print("Output file: ");
-		String outputFileName = console.next();
-		
-		File inputFile = new File(inputFileName);
-		Scanner in = new Scanner(inputFile);
-		PrintWriter out = new PrintWriter(outputFileName);
-		
-		while (in.hasNextDouble())
+		String Error = "Error opening file.";
+		try
 		{
-			double B = in.nextDouble();
-			double R = in.nextDouble();
-			double C = in.nextDouble();
-			double Starting_t = in.nextDouble();
-			double Ending_t = in.nextDouble();
-
-			double steps = (Ending_t - Starting_t ) / 100.0;
-			double RC = (1.0 / (R * C ));
-			double ans = 0.0;
+			Scanner console = new Scanner(System.in);
 			
-			for(double i = Starting_t; i <= Ending_t; i = i + steps)
+			System.out.print("Input file: ");
+			String inputFileName = console.next();
+			
+			System.out.print("Output file: ");
+			String outputFileName = console.next();
+			
+			File inputFile = new File(inputFileName);
+			Scanner in = new Scanner(inputFile);
+			PrintWriter out = new PrintWriter(outputFileName);
+			
+			while (in.hasNextDouble())
 			{
-				ans = B * (1 - Math.exp(-i*RC));
-				//System.out.println( +i  + " " +ans);
-				out.printf("%1f %1f\n",i,ans);
+				double B = in.nextDouble();
+				double R = in.nextDouble();
+				double C = in.nextDouble();
+				double Starting_t = in.nextDouble();
+				double Ending_t = in.nextDouble();
+	
+				double steps = (Ending_t - Starting_t ) / 100.0;
+				double RC = (1.0 / (R * C ));
+				double ans = 0.0;
+				
+				for(double i = Starting_t; i <= Ending_t; i = i + steps)
+				{
+					ans = B * (1 - Math.exp(-i*RC));
+					//System.out.println( +i  + " " +ans);
+					out.printf("%1f  %1f\n",i,ans);
+				}
+				
 			}
-			
+			in.close();
+			out.close();
 		}
-		in.close();
-		out.close();
+		catch (FileNotFoundException exception)
+		{
+			System.out.println(Error);
+		} 
+		catch(IOException exception) 
+		{
+			exception.printStackTrace();
+		}
 	}
 
 }
