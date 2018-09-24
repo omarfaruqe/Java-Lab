5f15d438e75d90fbfda5c26530c528cf2b6f3eff
arif <arifjaman325@gmail.com>
2018-09-24 10:23:54 +0600

    Hello

--- src/main/Circuit.java
+++ src/main/Circuit.java
@@ -1,5 +1,47 @@
 package main;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Circuit {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		final int TEN=10;
+		final int ZERO=0;
+		final int ONE=1;
+		final int MINUS=-1;
+		final double e=2.71828;
+		int i=ZERO;
+		double Voltage;
+		try{
+		
+		File  inputFile = new File ("params.txt");
+		PrintWriter out = new PrintWriter ("rr.txt");
+		Scanner in = new Scanner (inputFile);
+		
+        double B= in.nextDouble();
+		double R=in.nextDouble();
+	    double C= in.nextDouble();
+		int  ti=in.nextInt();
+		int  tf = in.nextInt();
+		for(i=ti;i<=tf;i=i+TEN) {
+			out.print(i);
+			out.print("  ");
+			Voltage = B*(ONE - Math.pow(e , (MINUS*i/(R*C))));
+			out.println(Voltage);
+		}
+		in.close();
+		out.close();
+		}
+		catch(IOException exception) {
+			System.out.println("Opps! Error Opening File!");
+		}
+
+	}
 
 }
+© 2018 GitHub, Inc.
+Terms
+Privacy
