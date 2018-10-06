d7fd40596f6f86512f79de111acfc7b17a17007b
Abdul Ahad Sheikh <ahadsheikh1999@gmail.com>
2018-10-06 22:29:46 +0600

    I found a cool frame

---
+++ src/shapecalc/ShapeCalc.java
@@ -0,0 +1,11 @@
+package shapecalc;
+
+public class ShapeCalc extends AFrame {
+	public ShapeCalc(String title) {
+		super(title);
+	}
+	protected void initialize() {
+		setSize(400,400);
+		setLocationRelativeTo(null);
+	}
+}
