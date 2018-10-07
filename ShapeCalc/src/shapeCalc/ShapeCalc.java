c12f996f4a750ba6d9cd4b8c9f87f2adddb77142
Arupcseru <arupcseru@gmail.com>
2018-10-07 20:03:52 +0600

    Create a Pop-Up window successfully

---
+++ ShapeCalc/src/shapeCalc/ShapeCalc.java
@@ -0,0 +1,19 @@
+package shapeCalc;
+
+public class ShapeCalc extends AFrame {
+
+	public ShapeCalc(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		setSize(400, 400); 
+		setLocationRelativeTo(null);
+		
+	}
+
+}
