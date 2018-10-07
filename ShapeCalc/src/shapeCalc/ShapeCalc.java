77f04cc37929644cff9ae386f39920dc1d22989d
Arupcseru <arupcseru@gmail.com>
2018-10-07 20:08:21 +0600

    Final commit on branch 1710876133-part1. Part1 is complete

--- ShapeCalc/src/shapeCalc/ShapeCalc.java
+++ ShapeCalc/src/shapeCalc/ShapeCalc.java
@@ -1,5 +1,16 @@
 package shapeCalc;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class ShapeCalc extends AFrame {
 
 	public ShapeCalc(String title) {
@@ -10,7 +21,30 @@ public class ShapeCalc extends AFrame {
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
+		final JTextField typeTF = new JTextField("Square");
+		JPanel ctrlPnl = new JPanel();
+		JButton newBtn = new JButton("New...");
+		JPanel displayPnl = new JPanel();
+		final JLabel areaLbl = new JLabel("Area:");;
+		ctrlPnl.add(typeTF);
+		ctrlPnl.add(newBtn);
+		Container cp = getContentPane();
+		cp.add(ctrlPnl, BorderLayout.NORTH);
+		cp.add(displayPnl, BorderLayout.CENTER);
+		cp.add(areaLbl, BorderLayout.SOUTH);
+		
+		newBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				
+				String text = typeTF.getText();
+				areaLbl.setText(text);
+				
+			}
+			
+		});
+		
 		setSize(400, 400); 
 		setLocationRelativeTo(null);
 		
