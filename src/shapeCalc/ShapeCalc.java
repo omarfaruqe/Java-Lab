06c5258572a861f76ab609cba43c037a8ae774e2
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2018-10-08 21:56:56 +0600

    Made my first Java GUI application

--- src/shapeCalc/ShapeCalc.java
+++ src/shapeCalc/ShapeCalc.java
@@ -1,5 +1,10 @@
 package shapeCalc;
 
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
 public class ShapeCalc extends AFrame {
 
 	public ShapeCalc(String title) {
@@ -11,7 +16,26 @@ public class ShapeCalc extends AFrame {
 	protected void initialize() {
 		setSize(400, 400);
 		setLocationRelativeTo(null);
+		JPanel ctrlPnl = new JPanel();
+		JPanel displayPnl = new JPanel();
+		JLabel areaLbl = new JLabel("Area: ");
 		
+		Container cp = getContentPane();
+		cp.add(ctrlPnl, BorderLayout.NORTH);
+		cp.add(displayPnl, BorderLayout.CENTER);
+		cp.add(areaLbl, BorderLayout.SOUTH);
+		
+		JTextField typeTF = new JTextField("Square");
+		JButton newBtn = new JButton("New ...");
+		
+		ctrlPnl.add(typeTF);
+		ctrlPnl.add(newBtn);
+		
+		newBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				areaLbl.setText(typeTF.getText());
+			}
+		});
 	}
 	
 }
