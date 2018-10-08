182aeedaaf0c8afc15d54101c9986c1c313ed5b8
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2018-10-08 21:25:12 +0600

    Made a simple window frame

---
+++ src/shapeCalc/ShapeCalc.java
@@ -0,0 +1,17 @@
+package shapeCalc;
+
+public class ShapeCalc extends AFrame {
+
+	public ShapeCalc(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initialize() {
+		setSize(400, 400);
+		setLocationRelativeTo(null);
+		
+	}
+	
+}
---
+++ src/shapeCalc/ShapeCalcController.java
@@ -0,0 +1,13 @@
+package shapeCalc;
+
+public class ShapeCalcController {
+	ShapeCalcController() {
+		ShapeCalc shapecalc = new ShapeCalc("Shape Calculator");
+		shapecalc.validate();
+		shapecalc.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new ShapeCalcController();
+	}
+}
