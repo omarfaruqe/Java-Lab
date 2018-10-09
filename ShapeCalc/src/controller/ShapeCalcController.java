022938e7f07e6ae649710597fec1d813671cfa05
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-10-09 20:07:09 +0600

    shape calculator validate and visiable

--- ShapeCalc/src/controller/ShapeCalcController.java
+++ ShapeCalc/src/controller/ShapeCalcController.java
@@ -1,11 +1,16 @@
 package controller;
 
+import shapeCalc.ShapeCalc;
 
 public class ShapeCalcController {
 	
+	ShapeCalc shapeCalc;
 	
 	public ShapeCalcController(String title) {
 		
+		shapeCalc =new ShapeCalc(title);
+		shapeCalc.validate();
+		shapeCalc.setVisible(true);
 		
 	}
 	
