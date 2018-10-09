6a7e902682c53567fa2cbd37d26ba6f2e71d86b1
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-10-09 19:55:05 +0600

    constructor and main mathod  added to the class shapeCalcControler

--- ShapeCalc/src/controller/ShapeCalcController.java
+++ ShapeCalc/src/controller/ShapeCalcController.java
@@ -1,5 +1,15 @@
 package controller;
 
 public class ShapeCalcController {
+	
+	public ShapeCalcController(String title) {
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		new ShapeCalcController("Shape Calculator");	
+	}
 
 }
