757a4fd71f9a2645d9e1f89cf6d10d32ed615a1b
Manik Hosen <rahathossenmanik@gmail.com>
2018-10-09 16:25:37 +0600

    Part1 have been completed.And they have been saperated in a folder called Part1.

---
+++ ShapeCalc/Part1/controller/ShapeCalcController.java
@@ -0,0 +1,15 @@
+package controller;
+import shapeCalc.ShapeCalc;
+
+public class ShapeCalcController {
+	String text="Hello";
+	private ShapeCalc shapeCalc = new ShapeCalc("This is Shape Calculator");
+	ShapeCalcController(){
+		shapeCalc.validate();
+		shapeCalc.setVisible(true);
+	}
+
+	public static void main(String[] args){
+		new ShapeCalcController();
+	}
+}
