2fc3b6f2c1982b1e227ebe1ff0707889dee82cd3
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-10-09 19:56:16 +0600

    AFrame class completed

--- ShapeCalc/src/shapeCalc/AFrame.java
+++ ShapeCalc/src/shapeCalc/AFrame.java
@@ -1,5 +1,19 @@
 package shapeCalc;
 
-public class AFrame {
+import javax.swing.*;  
 
+public abstract class AFrame extends JFrame {
+
+    public AFrame(String title) {
+        super(title);
+        
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent e) {
+                System.out.println(e);
+                System.exit(0);
+            }
+        });
+        initialize();
+    }
+    protected abstract void initialize();
 }
