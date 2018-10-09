ab93e7defa57c839a4bc45f82a2ded18a6b26165
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-10-09 20:28:16 +0600

    Button & Panel added to the GUI

--- ShapeCalc/src/shapeCalc/ShapeCalc.java
+++ ShapeCalc/src/shapeCalc/ShapeCalc.java
@@ -1,5 +1,13 @@
 package shapeCalc;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class ShapeCalc  extends AFrame {
 
 	public ShapeCalc(String title) {
@@ -9,5 +17,23 @@ public class ShapeCalc  extends AFrame {
 	
 	protected void initialize() {
 		
+		JTextField typeTF = new JTextField("Square");
+		JButton newBtn = new JButton("New..");
+		
+		JPanel ctrlPnl = new JPanel();
+		JPanel displayPnl = new JPanel();
+		JLabel areaLbl = new JLabel("Area:"); 
+		
+		Container cp = getContentPane();
+		
+		ctrlPnl.add(typeTF);
+		ctrlPnl.add(newBtn);
+		
+		cp.add(ctrlPnl, BorderLayout.NORTH);
+		cp.add(displayPnl, BorderLayout.CENTER);
+		cp.add(areaLbl, BorderLayout.SOUTH);
+		
+		setSize(400, 400); 
+		setLocationRelativeTo(null);
 	}
 }
