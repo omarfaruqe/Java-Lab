1fd93384bdcac9611d706f8e7712e2429cfa4382
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-10-09 20:03:51 +0600

    ShapeCalc class added

--- ShapeCalc/src/controller/ShapeCalcController.java
+++ ShapeCalc/src/controller/ShapeCalcController.java
@@ -1,6 +1,8 @@
 package controller;
 
+
 public class ShapeCalcController {
+	
 	
 	public ShapeCalcController(String title) {
 		
---
+++ ShapeCalc/src/shapeCalc/ShapeCalc.java
@@ -0,0 +1,13 @@
+package shapeCalc;
+
+public class ShapeCalc  extends AFrame {
+
+	public ShapeCalc(String title) {
+		
+		super(title);	
+	}
+	
+	protected void initialize() {
+		
+	}
+}
