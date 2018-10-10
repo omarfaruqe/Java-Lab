636f82d7abd92603a2e635dda82129c415656592
Mamun cse <almamuncseru@gmail.com>
2018-10-10 13:07:46 +0600

    Shape Calculator Part2

---
+++ ShapeCalc/src/ShapeCalc/shapes/SquareFactory.java
@@ -0,0 +1,16 @@
+package ShapeCalc.shapes;
+import java.awt.Graphics;
+
+public class SquareFactory {
+	static SquareFactory SquareMaker;
+	public static SquareFactory getSquareMaker(){
+		new IShape() {
+			public void paint(Graphics g, int x, int y) {
+ 			}  
+		};
+	}
+	public static void main(String[] args) {
+		new SquareFactory();
+	}
+
+}
--- ShapeCalc/src/controller/ShapeCalcController.java
+++ ShapeCalc/src/controller/ShapeCalcController.java
@@ -1,5 +1,17 @@
 package controller;
+import ShapeCalc.ShapeCalc;
+import ShapeCalc.shapes.SquareFactory;
 
 public class ShapeCalcController {
+	String text="Hello";
+	private ShapeCalc shapeCalc = new ShapeCalc("This is Shape Calculator");
+	ShapeCalcController(){
+		shapeCalc.validate();
+		shapeCalc.setVisible(true);
+	}
+ 	public static void main(String[] args){
+		new ShapeCalcController();
+		new SquareFactory();
+	}
 
 }
