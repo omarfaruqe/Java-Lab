2524b2c9421261ecb2092d965a1e3f7a32cc48d0
Rakib <rakib.rscse@gmail.com>
2018-10-10 12:07:34 +0600

    Nullfacotry ,rectangle factory and square factory class is added

---
+++ The Shape Calculator/src/shapeCalc/shapes/RectangleFactory.java
@@ -0,0 +1,19 @@
+package shapeCalc.shapes;
+
+import javax.swing.JPanel;
+import shapeCalc.IShape;
+import shapeCalc.IShapeFactory;
+
+public class RectangleFactory
+  implements IShapeFactory
+{
+  public JPanel getPropertyPanel()
+  {
+    return null;
+  }
+
+  public IShape makeShape()
+  {
+    return null;
+  }
+}
