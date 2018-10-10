2bb08c07247d2efff14dd4203f5905f24d7a99a4
Apple Mahmud <applecse25@gmail.com>
2018-10-10 07:58:50 +0600

    part1 complete

--- ShapeCalculator/src/shapecalc/IShape.java
+++ ShapeCalculator/src/shapecalc/IShape.java
@@ -1,5 +1,8 @@
 package shapecalc;
 
-public interface IShape {
+import java.awt.Graphics;
 
+public interface IShape {
+	public abstract double getArea();
+    public abstract void paint(Graphics g, int x, int y);
 }
