a5a074ac99e670965a42a30c94f8eae6737fc92d
arif <arifjaman325@gmail.com>
2018-10-13 21:37:03 +0600

    final commit

--- ShapeCalc/src/controller/ShapeCalcController.java
+++ ShapeCalc/src/controller/ShapeCalcController.java
@@ -1,5 +1,14 @@
-package controller;
+package shapeCalc;
 
 public class ShapeCalcController {
-
+	private ShapeCalc _shapeCalc;
+	public ShapeCalcController() {
+		this._shapeCalc = new ShapeCalc("Shape Calculator");
+		this._shapeCalc.validate();
+		this._shapeCalc.setVisible(true);
+	}
+	public static void main(String[] args)
+	{
+	   new ShapeCalcController();
+	}
 }
