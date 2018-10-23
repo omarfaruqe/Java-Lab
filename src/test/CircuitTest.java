8afb926fbb6c3e80bd1a7572398306397f4a9e91
Shohan-Pramanik <shohan.pramanik38@gmail.com>
2018-10-23 14:51:57 +0600

    LabTest1

---
+++ src/test/CircuitTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.Circuit;
+
+class CircuitTest {
+
+	void circuitTest() {
+		Circuit circuit = new Circuit();
+	}
+
+}
